8cd885ea138c8ac3155af22554a53c2ea0374468
YotamAlon <yotamalon@gmail.com>
2019-07-14 07:09:29 +0300

    Fixed User-Player relation

--- Models/Player.py
+++ Models/Player.py
@@ -1,8 +1,9 @@
-from peewee import ForeignKeyField, CharField
+from peewee import Model, ForeignKeyField, CharField
 from Models.User import User
 from Models.GoGame import GoGame
 
 
-class Player(User):
+class Player(Model):
+    user = ForeignKeyField(User)
     go_game = ForeignKeyField(GoGame, backref='players')
     color = CharField()
