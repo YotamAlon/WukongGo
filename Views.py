6f62e1fb687d428eb48e38741b56739d35030a3d
YotamAlon <yotamalon@gmail.com>
2019-07-19 16:14:14 +0300

    Added black and white pieces images

--- Views.py
+++ Views.py
@@ -1,4 +1,20 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty, StringProperty, ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+
+
+class Piece(Button):
+    index = NumericProperty(None)
+    img = StringProperty(None)
+
+    def place_piece(self, color):
+        self.img = 'assets/' + color + '.png'
+
+    def on_press(self):
+        if self.parent.is_move_legal(self.index):
+            color = self.parent.make_move(self.index)
+            self.place_piece(color)
 
 
 class MenuScreen(Screen):
@@ -6,6 +22,29 @@ class MenuScreen(Screen):
         self.manager.handle_click(signal)
 
 
+class GameBoard(GridLayout):
+    board_size = NumericProperty(19)
+    current_player = StringProperty('black')
+
+    def __init__(self, **kwargs):
+        super(GameBoard, self).__init__(**kwargs)
+        for i in range(self.board_size ** 2):
+            self.add_widget(Piece(index=i))
+
+    def is_move_legal(self, index):
+        return True
+
+    def make_move(self, move):
+        if self.current_player == 'black':
+            self.current_player = 'white'
+            return 'white'
+        else:
+            self.current_player = 'black'
+            return 'black'
+
+
 class GameScreen(Screen):
+    board = ObjectProperty(None)
+
     def handle_click(self, signal):
         self.manager.handle_click(signal)
